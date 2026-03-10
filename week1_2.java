c8654381be67397b5157a666d8ea10372905ea3c
anshika-om <ao3051@srmist.edu.in>
2026-03-10 12:44:41 +0530

    average marks

--- week1_2.java
+++ week1_2.java
@@ -1,8 +1,9 @@
 class week1_2 {
     public static void main(String[] args) {
-        int birthYear = 2000;
-        int currentYear = 2024;
-        int age = currentYear - birthYear;
-        System.out.println("Harry's age in 2024 is " + age);
+        int maths = 94;
+        int physics = 95;
+        int chemistry = 96;
+        double average = (maths + physics + chemistry) / 3.0;
+        System.out.println("Sam’s average mark in PCM is " + average);
     }
 }
