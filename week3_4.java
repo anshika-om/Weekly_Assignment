361dbc693b9c3cf8c9711356860f99169545838e
anshika-om <ao3051@srmist.edu.in>
2026-03-10 13:50:43 +0530

    find odd or even number

--- week3_4.java
+++ week3_4.java
@@ -1,18 +1,25 @@
 import java.util.Scanner;
 
 public class Week3_4 {
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int number;
         System.out.print("Enter a number: ");
         number = sc.nextInt();
-        if (number > 0) {   // natural number check
-            int sum = number * (number + 1) / 2;
-            System.out.println("The sum of " + number + " natural numbers is " + sum);
+        if (number > 0) { 
+            for (int i = 1; i <= number; i++) {
+                if (i % 2 == 0) {
+                    System.out.println(i + " is an even number");
+                } else {
+                    System.out.println(i + " is an odd number");
+                }
+            }
         } else {
             System.out.println("The number " + number + " is not a natural number");
         }
         sc.close();
     }
 }
+
 
