f5c928627446315d3c609174ac1ee75d092d113d
anshika-om <ao3051@srmist.edu.in>
2026-03-10 12:47:16 +0530

    distance from km to miles

--- week1_2.java
+++ week1_2.java
@@ -1,9 +1,9 @@
 class week1_2 {
     public static void main(String[] args) {
-        int maths = 94;
-        int physics = 95;
-        int chemistry = 96;
-        double average = (maths + physics + chemistry) / 3.0;
-        System.out.println("Sam’s average mark in PCM is " + average);
+        double kilometers = 10.8;
+        double conversionFactor = 1.6;
+        double miles = kilometers * conversionFactor;
+        System.out.println("The distance " + kilometers + 
+                           " km in miles is " + miles);
     }
 }
