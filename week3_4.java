2542e8e3aa12609dca9618a18bd8539f3150afae
anshika-om <ao3051@srmist.edu.in>
2026-03-10 13:48:19 +0530

    sum of n natural number

--- week3_4.java
+++ week3_4.java
@@ -1,30 +1,16 @@
 import java.util.Scanner;
 
 public class Week3_4 {
-   
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int number1, number2, number3;
-        System.out.print("Enter first number: ");
-        number1 = sc.nextInt();
-        System.out.print("Enter second number: ");
-        number2 = sc.nextInt();
-        System.out.print("Enter third number: ");
-        number3 = sc.nextInt();
-        if (number1 > number2 && number1 > number3) {
-            System.out.println("Is the first number the largest? Yes");
+        int number;
+        System.out.print("Enter a number: ");
+        number = sc.nextInt();
+        if (number > 0) {   // natural number check
+            int sum = number * (number + 1) / 2;
+            System.out.println("The sum of " + number + " natural numbers is " + sum);
         } else {
-            System.out.println("Is the first number the largest? No");
-        }
-        if (number2 > number1 && number2 > number3) {
-            System.out.println("Is the second number the largest? Yes");
-        } else {
-            System.out.println("Is the second number the largest? No");
-        }
-        if (number3 > number1 && number3 > number2) {
-            System.out.println("Is the third number the largest? Yes");
-        } else {
-            System.out.println("Is the third number the largest? No");
+            System.out.println("The number " + number + " is not a natural number");
         }
         sc.close();
     }
