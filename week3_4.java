dc7f2a5c6ab35e7972660eb372d73ec31f61b8da
anshika-om <ao3051@srmist.edu.in>
2026-03-10 13:24:26 +0530

    divisible by 5

---
+++ week3_4.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class week3_4 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int number;
+
+        System.out.print("Enter a number: ");
+        number = sc.nextInt();
+
+        if (number % 5 == 0) {
+            System.out.println("Is the number " + number + " divisible by 5? Yes");
+        } else {
+            System.out.println("Is the number " + number + " divisible by 5? No");
+        }
+
+        sc.close();
+    }
+}
